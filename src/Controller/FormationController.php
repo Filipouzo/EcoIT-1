6e9f829b51f005a81b5ceb74aac984b055ed7c3a
Marie Defrance <defrance_marie@yahoo.fr>
2022-03-25 16:48:26 +0100

    Starting TWIG template: Formations page

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
+use App\Entity\Section;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class FormationController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/formation', name: 'app_formation')]
     public function index(): Response
     {
+        $formations = $this->entityManager->getRepository(Formation::class)->findAll();
+        // $formations = $this->entityManager->getRepository(Section::class)->findAll();
+        
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations
         ]);
     }
 }
