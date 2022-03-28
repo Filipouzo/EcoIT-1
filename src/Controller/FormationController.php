1fe614438b0ba9cb716d646a4e6bbfd7a8f8ca48
Marie Defrance <defrance_marie@yahoo.fr>
2022-03-28 16:34:31 +0200

    Add a follow formation fonctionality v1

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Entity\Lesson;
 use App\Entity\Section;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +27,31 @@ class FormationController extends AbstractController
         
         return $this->render('formation/index.html.twig', [
             'formations' => $formations
+        ]);
+    }
+
+    #[Route('/suivre-formation/{slug}', name: 'formation')]
+    public function show($slug): Response
+    {
+        
+        $formation = $this->entityManager->getRepository(Formation::class)->findOneBySlug($slug);
+
+        // Find ID formation
+        $id_formation = $formation->getId();
+        $section = $this->entityManager->getRepository(Section::class)->findOneByFormation($id_formation);
+
+        // Find ID section
+        $id_section = $section->getId();
+        $lessons = $this->entityManager->getRepository(Lesson::class)->findBy(['section' => $id_section]);
+
+        if (!$formation) {
+            return $this->redirectToRoute('app_formation');
+        }
+
+        return $this->render('formation/show.html.twig', [
+            'formation' => $formation,
+            'section' => $section,
+            'lessons' => $lessons,
         ]);
     }
 }
